0893461c524e2d43c221fd9e64730659b9d7b1ba
= <Phi@gold.ratio>
2016-06-13 22:01:14 +0000

    Added comment

--- seq2seq_model.py
+++ seq2seq_model.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+#Modified my Intangere to use embedding instead of attention
 
 """Sequence-to-sequence model with embedding, no attention"""
 
